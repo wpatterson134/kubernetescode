8f8a07bbbdcf70f2654a0511a846f65de74ba26a
Will Patterson <50717207+wpatterson134@users.noreply.github.com>
2023-11-08 19:50:46 -0800

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Please subscribe, like, and comment on this video, TY!!!'
+    return 'It's over Anakin! I have the high ground!!!'
